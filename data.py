9eb68be7562da880a8ef439b087511174477f684
lxotsi <s.saeedalhajeri@gmail.com>
2024-12-02 12:20:32 +0400

    Naive Bayes Model

--- data.py
+++ data.py
@@ -1,6 +1,11 @@
 import kagglehub
 
+
+# Kaggle Link: https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset?resource=download
+
 # Download latest version
 path = kagglehub.dataset_download("uciml/sms-spam-collection-dataset")
 
 print("Path to dataset files:", path)
+
+# Directory Output: /Users/vanilla/.cache/kagglehub/datasets/uciml/sms-spam-collection-dataset/versions/1
